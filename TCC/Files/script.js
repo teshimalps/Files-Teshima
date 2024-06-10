f461c50ec5201d11b861c9f0fed54bef67812eea
Teshima <149195365+teshimalps@users.noreply.github.com>
2024-06-10 18:00:47 -0300

    Add files via upload

---
+++ TCC/Files/script.js
@@ -0,0 +1,32 @@
+window.sr = ScrollReveal({ reset: true});
+
+sr.reveal('.area-1', { origin: "top", distance: "1rem", duration:2000, reset: false});
+sr.reveal('.area-2', { origin: "top", distance: "1rem", duration:2000, reset: false });
+sr.reveal('.area-3', { origin: "top", distance: "1rem", duration:2000, reset: false });
+sr.reveal('.area-4', { origin: "top", distance: "1rem", duration:2000, reset: false });
+sr.reveal('.area-5', { origin: "top", distance: "1rem", duration:2000, reset: false });
+sr.reveal('.area-6', { origin: "top", distance: "1rem", duration:2000, reset: false });
+sr.reveal('.area-7', { origin: "top", distance: "1rem", duration:2000, reset: false });
+sr.reveal('.area-8', { origin: "top", distance: "1rem", duration:2000, reset: false });
+
+sr.reveal('.area-9', { origin: "left", distance: "2rem", duration:2000, reset: false });
+sr.reveal('.area-10', { origin: "left", distance: "0.5rem", duration:2000, reset: false });
+sr.reveal('.area-11', { origin: "left", distance: "1rem", duration:2000, reset: false });
+
+sr.reveal('.area-12', { origin: "bottom", distance: "1rem", duration:2000, reset: false });
+
+sr.reveal('.area-13', { origin: "bottom", distance: "1rem", duration:2000, reset: false });
+sr.reveal('.area-14', { origin: "", distance: "1rem", duration:1000, reset: false });
+sr.reveal('.area-15', { origin: "", distance: "1rem", duration:1000, reset: false });
+sr.reveal('.area-16', { origin: "", distance: "1rem", duration:1000, reset: false });
+sr.reveal('.area-17', { origin: "", distance: "1rem", duration:1000, reset: false });
+sr.reveal('.area-18', { origin: "", distance: "1rem", duration:1000, reset: false });
+sr.reveal('.area-19', { origin: "", distance: "1rem", duration:1000, reset: false });
+sr.reveal('.area-20', { origin: "", distance: "1rem", duration:1000, reset: false });
+sr.reveal('.area-21', { origin: "", distance: "1rem", duration:1000, reset: false });
+sr.reveal('.area-22', { origin: "", distance: "1rem", duration:1000, reset: false });
+sr.reveal('.area-23', { origin: "", distance: "1rem", duration:1000, reset: false });
+sr.reveal('.area-24', { origin: "", distance: "1rem", duration:1000, reset: false });
+sr.reveal('.area-25', { origin: "", distance: "1rem", duration:1000, reset: false });
+sr.reveal('.area-26', { origin: "", distance: "1rem", duration:1000, reset: false });
+sr.reveal('.area-27', { origin: "", distance: "1rem", duration:1000, reset: false });
